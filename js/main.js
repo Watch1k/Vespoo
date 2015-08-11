d8b0e0af7d1db333c0768ec53be28865753899f3
unknown <host.sf@gmail.com>
2015-08-12 00:44:27 +0300

    start city

--- js/main.js
+++ js/main.js
@@ -14,6 +14,76 @@ $(document).ready(function(){
 		});
 	});
 
+// region list
+	var regionArr = [];
+	var regionMark;
+	$.ajax({
+		type: "GET",
+		url: "ua-cities.xml", // change to full path of file on server
+		dataType: "xml",
+		success: parseRegion,
+		complete: setupRegion,
+		error: function() {
+			// if XML File could not be found
+		}
+	});
+	function parseRegion(xml) {
+		//find every query value
+		$(xml).find("region").each(function() {
+			regionArr.push($(this).attr("name"));
+		});
+	}
+	function setupRegion() {
+	    $('.region-wrap').append('<ul class="region-list"></ul>');
+	    for (var i = 0; i < regionArr.length; i++) {
+	    	$('.region-list').append('<li></li>');
+	    	// regionArr[i]
+	    	$('.region-list li:last-child').text(regionArr[i]);
+	    };
+		$('.region-list li').on('click', function(){
+			regionMark = $(this).text();
+			cityList();
+		});
+	}
+
+// city list
+	function cityList() {
+		var cityArr = [];
+		$.ajax({
+			type: "GET",
+			url: "ua-cities.xml", // change to full path of file on server
+			dataType: "xml",
+			success: parseCity,
+			complete: setupCity,
+			error: function() {
+				// if XML File could not be found
+			}
+		});
+		function parseCity(xml) {
+			//find every query value
+			var regionCurrent;
+			$(xml).find("region").each(function(){
+				if ($(this).attr("name") == regionMark) {
+					regionCurrent = $(this);
+				};
+			});
+			$(xml).find(regionCurrent).find("city").each(function() {
+				cityArr.push($(this).attr("name"));
+			});
+		}
+		function setupCity() {
+		    $('.city-wrap').append('<ul class="city-list"></ul>');
+		    for (var i = 0; i < cityArr.length; i++) {
+		    	$('.city-list').append('<li></li>');
+		    	// cityArr[i]
+		    	$('.city-list li:last-child').text(cityArr[i]);
+		    };
+		    $('.city-list li').on('click', function(){
+				$('#cityBox').val($(this).text());
+			});
+		}
+	};
+
 // city autocomplete
 	var myArr = [];
 	$.ajax({
