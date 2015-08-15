8d0c6d0c75cc14eeefad613335d2ed8e8fda3ae0
unknown <host.sf@gmail.com>
2015-08-15 20:20:36 +0300

    watermark ratio

--- watermark.php
+++ watermark.php
@@ -18,19 +18,40 @@ $image = imagecreatefromjpeg($image_path);
 if ($image === false) {
     return false;
 }
-$size = getimagesize($image_path);
+
+// получаем размеры изображения
+list($width, $height) = getimagesize($image_path);
+
+// получение новых размеров
+$percent = 0.2;
+$ratio = $watermark_width / $watermark_height;
+if ($height >= $width) {
+	$new_width = $width * $percent;
+	$new_height = $new_width / $ratio;
+} else {
+	$new_height = $height * $percent;
+	$new_width = $new_height * $ratio;
+}
+
 // помещаем водяной знак на изображение
-$dest_x = $size[0] - $watermark_width - 5;
-$dest_y = $size[1] - $watermark_height - 5;
+$dest_x = $width - $new_width - 10;
+$dest_y = $height - $new_height - 10;
+
+// ресэмплирование
+$watermark_new = imagecreatetruecolor($new_width, $new_height);
+$trans_colour = imagecolorallocatealpha($watermark_new, 0, 0, 0, 127);
+imagefill($watermark_new, 0, 0, $trans_colour);
+imagecopyresampled($watermark_new, $watermark, 0, 0, 0, 0, $new_width, $new_height, $watermark_width, $watermark_height);
 
 imagealphablending($image, true);
-imagealphablending($watermark, true);
+imagealphablending($watermark_new, true);
 // создаём новое изображение
-imagecopy($image, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height);
+imagecopy($image, $watermark_new, $dest_x, $dest_y, 0, 0, $new_width, $new_height);
 imagejpeg($image);
 
 // освобождаем память
 imagedestroy($image);
 imagedestroy($watermark);  
+imagedestroy($watermark_new);
 
 ?>
